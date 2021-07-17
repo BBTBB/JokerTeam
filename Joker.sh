b5f1ac32bf5f871044b8fcf8888a866a87a12d74
BBTBB <abassbah43@gmail.com>
2021-07-17 22:45:41 +0300

    Update Joker.sh

--- Joker.sh
+++ Joker.sh
@@ -1,23 +1,4 @@
 #!/usr/bin/env bash
-cd $HOME/JokerTeam
-token="1613766929:AAFqID_sLdbF4G2c48KVw6o0H2YRExDzfZk"
-while(true) do
-rm -fr ../.telegram-cli
-if [ ! -f ./tg ]; then
-echo "┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉"
-echo "~ The tg File Was Not Found In The Bot Files"
-echo "┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉"
-exit 1
-fi
-if [ ! $token ]; then
-echo "┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉"
-echo "~ The Token Was Not Found In The config.lua File"
-echo "┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉"
-exit 1
-fi
-./tg -s ./Joker.lua -p PROFILE --bot=$token
-done
-#!/usr/bin/env bash
 THIS_DIR=$(cd $(dirname $0); pwd)
 Version=`lsb_release -rs | cut -f1 -d"."`
 install() {
