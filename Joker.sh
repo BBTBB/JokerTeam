c1a205f47cd45c564e38e88025b5f482aae63bf8
BBTBB <abassbah43@gmail.com>
2021-07-27 03:22:41 +0300

    Add files via upload

---
+++ Joker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+cd $HOME/JokerTeam
+token="1613766929:AAFh6Yfovq0mntZt6CdxNLKvWcP-YU3NBSY"
+while(true) do
+rm -fr ../.telegram-cli
+if [ ! -f ./tg ]; then
+echo "┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉"
+echo "~ The tg File Was Not Found In The Bot Files!"
+echo "┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉"
+exit 1
+fi
+if [ ! $token ]; then
+echo "┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉"
+echo "~ The Token Was Not Found In The config.lua File!"
+echo "┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉ ≈ ┉"
+exit 1
+fi
+./tg -s ./Joker.lua -p PROFILE --bot=$token
+done
